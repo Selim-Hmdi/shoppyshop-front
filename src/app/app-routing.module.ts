0523e8e2d0ef059c857fcdb279ec548464c50afe
Camille <hermelcamille@gmail.com>
2022-06-14 14:07:30 +0200

    create pages by category and access on homepage + add searchbar on homepage with filter on articles(mark, description and category) + improve header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { CategoryComponent } from './category/category.component';
 
 const routes: Routes = [
-  { path: '',  component: HomeComponent },
-  { path: 'connexion',  component: ConnexionComponent },
-  { path: 'inscription',  component: InscriptionComponent },
+  { path: '', component: HomeComponent },
+  { path: 'connexion', component: ConnexionComponent },
+  { path: 'inscription', component: InscriptionComponent },
+  { path: 'article/:categorie', component: CategoryComponent },
 ];
 
 @NgModule({
