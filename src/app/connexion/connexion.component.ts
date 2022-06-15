c07eb5b3d2bf2ff315cc436d5d726ab69d0ffcb8
amale <aidrissi001@gmail.com>
2022-06-15 12:31:36 +0000

    modif du html de la page connexion avec l'ajout de ngsubmit + logique métier ajoutée dans le ts

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserserviceService } from '../userservice.service';
+import { Router } from '@angular/router';
+import { User } from '../user';
 
 @Component({
   selector: 'app-connexion',
@@ -6,13 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./connexion.component.css']
 })
 export class ConnexionComponent implements OnInit {
-
-  constructor() { }
+  email: string;
+  password: string;
+  errorMessage = "";
+  user: any;
+  constructor(private userService: UserserviceService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  connect() {
+  onSubmit() {
+    this.userService.getUserByEmailAndPassword(this.email, this.password).subscribe(
+      data => {
+        this.user = data;
+        console.log(this.user);
+        this.goToHomePage();
+      }, (error) => {
+        if (this.user == null) {
+          this.errorMessage = "Utilisateur introuvable. L'email ou le mot de passe entré est inexistant ou incorrect";
+        } else {
+          this.errorMessage = error;
+        }
+      });
+  }
 
+  goToHomePage() {
+    this.router.navigate(['']);
   }
 }
+
