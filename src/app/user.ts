ca99a5626ddae1ceba13894020ee63ceb62d3ad6
Selim Hoummadi <selim_hoummadi@hotmail.com>
2022-06-16 13:58:09 +0200

    Fixed errors due to some user's properties missing

--- src/app/user.ts
+++ src/app/user.ts
@@ -2,6 +2,10 @@ export class User {
   id: number;
   nom: string;
   prenom: string;
+  adresse: string;
+  telephone: string;
+  ville: string;
+  pays: string;
   email: string;
   password: string;
 }
